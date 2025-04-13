fe5e51701741e2301b7187932eee14ed065be806
arcolinuxz <arcolinuxinfo@gmail.com>
2025-04-13 20:12:15 +0200

    farewell

---
+++ calamares/calamares-3.3.14.250121-3/dep-calamares.sh
@@ -0,0 +1,31 @@
+sudo pacman -S --needed --noconfirm kconfig
+sudo pacman -S --needed --noconfirm kcoreaddons
+sudo pacman -S --needed --noconfirm kiconthemes
+sudo pacman -S --needed --noconfirm ki18n
+sudo pacman -S --needed --noconfirm kio
+sudo pacman -S --needed --noconfirm solid
+sudo pacman -S --needed --noconfirm yaml-cpp
+sudo pacman -S --needed --noconfirm kpmcore
+yay -S mkinitcpio-openswap --noconfirm
+sudo pacman -S --needed --noconfirm boost-libs
+yay -S ckbcomp --noconfirm
+sudo pacman -S --needed --noconfirm hwinfo
+sudo pacman -S --needed --noconfirm qt5-svg
+sudo pacman -S --needed --noconfirm polkit-qt5
+sudo pacman -S --needed --noconfirm gtk-update-icon-cache
+sudo pacman -S --needed --noconfirm plasma-framework5
+sudo pacman -S --needed --noconfirm qt5-xmlpatterns
+sudo pacman -S --needed --noconfirm squashfs-tools
+
+sudo pacman -S --needed --noconfirm extra-cmake-modules
+sudo pacman -S --needed --noconfirm qt5-tools
+sudo pacman -S --needed --noconfirm qt5-translations
+sudo pacman -S --needed --noconfirm git
+sudo pacman -S --needed --noconfirm boost
+sudo pacman -S --needed --noconfirm kparts
+sudo pacman -S --needed --noconfirm kdbusaddons
+sudo pacman -S --needed --noconfirm appstream-qt
+sudo pacman -S --needed --noconfirm ninja
+sudo pacman -S --needed --noconfirm python-jsonschema
+sudo pacman -S --needed --noconfirm python-pyaml
+sudo pacman -S --needed --noconfirm doxygen
